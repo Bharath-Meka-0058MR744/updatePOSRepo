909ac611f861d00d3fc844a4fa0d1471f83fb976
Bharath <Bharath.Meka@softwareag.com>
2025-03-19 09:32:39 +0530

    first commit

---
+++ pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://SAG-FVDCSG3:5555/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "18-03-2025 21:52:16 IST";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"UpdatePOSShopify","createdDate":"18-03-2025 17:41:11 IST","version":"1.0"};
+}
+});
